60dbd43c4c0cea854faecdec83397ba3e3149f52
Yu Takeuchi <yu_takeuchi@narutech.co.jp>
2025-05-29 13:38:51 +0900

    TypeScriptバージョンに完全移行

--- src/__mocks__/supabaseClient.js
+++
@@ -1,8 +0,0 @@
-//自動テスト実行時用のモック
-export const supabase = {
-    from: () => ({
-        select: () => ({ data: [], error: null }),
-        insert: () => ({ data: [], error: null }),
-        delete: () => ({ data: [], error: null }),
-    }),
-};
